514c9ae71f3eeaa45294099f0800036bc0d3c298
Shivendran Tiruchanpalli <Shivendran_Tiruchanpalli@eogresources.com>
2021-02-11 10:22:18 -0600

    Updates

---
+++ precache-manifest.1e826f8d2fadc367bbe2c7c7f051dddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee382007f37c2cb2c4ed14c3e0b39c31",
+    "url": "/wedding-invite/index.html"
+  },
+  {
+    "revision": "662808194d90fbd0d43d",
+    "url": "/wedding-invite/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "3bfb29857c3eff7ef256",
+    "url": "/wedding-invite/static/css/main.ae73ef39.chunk.css"
+  },
+  {
+    "revision": "662808194d90fbd0d43d",
+    "url": "/wedding-invite/static/js/2.2eefcf6e.chunk.js"
+  },
+  {
+    "revision": "e0e1364a4cfdfbab90b2840523c79c0b",
+    "url": "/wedding-invite/static/js/2.2eefcf6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfb29857c3eff7ef256",
+    "url": "/wedding-invite/static/js/main.959c8d2d.chunk.js"
+  },
+  {
+    "revision": "c0949d3362e0b93e8139",
+    "url": "/wedding-invite/static/js/runtime-main.c3667dc6.js"
+  }
+]);
